12214af8d8b20f72b9a9dc497742c8c0c1088310
Koller Kevin <kevinkekoller1@gmail.com>
2024-03-29 15:54:02 +0100

    16x16

--- script.js
+++ script.js
@@ -5,8 +5,9 @@ for (let i = 0; i < 256; i++) {
     const div = document.createElement("div");
     div.classList.add("square");
     div.style.backgroundColor = "blue";
-    div.style.height = "100px";
-    div.style.width = "100px";
+    div.style.height = "50px";
+    div.style.width = "50px";
+    div.style.border = "5px solid black";
     container.appendChild(div); 
 }
 
