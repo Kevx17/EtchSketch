505ad368c76600bc7c706422f5d3decbde0d528c
Koller Kevin <kevinkekoller1@gmail.com>
2024-03-30 18:22:22 +0100

    the gode can generate the grid based on user input and interacts

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 const container = document.querySelector("#container");
-
-
-for (let i = 0; i < 256; i++) {
+//variable that stores the number of squares per side
+let n = prompt("enter a number for squares per side");
+//variable for the width and height(container is 960x960)
+let size = 960/n;
+for (let i = 0; i < n*n; i++) {
     const div = document.createElement("div");
     div.classList.add("square");
-    div.style.backgroundColor = "blue";
-    div.style.height = "50px";
-    div.style.width = "50px";
-    div.style.border = "5px solid black";
+    div.style.width = size + "px";
+    div.style.height = size + "px";
     container.appendChild(div); 
 }
 
